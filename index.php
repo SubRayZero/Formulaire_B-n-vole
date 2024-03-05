5e8804d333e27fd61cbb506114c00f90234ab3f7
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-03-05 14:17:01 +0100

    start css

--- index.php
+++ index.php
@@ -5,16 +5,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Formulaire Bénévole</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/index.css">
     <script src="annonces.js" type="module"></script>
 </head>
 
 <body>
 
-    <header>
+    <header class="header">
+        <div class="header_nav_bar">header</div>
     </header>
 
-    <a href=""
+    <main>
+        <section class="first_section">
+            <div class="first_section_title">Devenez bénévole !</div>
+            <div class="first_section_images"></div>
+        </section>
+
+        <section class="card_section">
+    </main>
 
 </body>
+
 </html>
