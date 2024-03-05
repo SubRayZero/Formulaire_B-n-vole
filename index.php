04860019080fe71250c617f6bc06817db2b87408
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-03-05 16:44:16 +0100

    Add - footer

--- index.php
+++ index.php
@@ -64,6 +64,12 @@
         </div>
     </div>
 
+    <footer>
+        <div class="footer_nav_bar">Mentions légales</div>
+        <div class="footer_nav_bar">CGU</div>
+        <div class="footer_nav_bar">Plan du site</div>
+    </footer>
+
 </body>
 
 </html>
