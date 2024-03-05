23b0e61e20fadcc9cb9087c6caadb9892ff9c533
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-03-05 16:21:25 +0100

    Fix - homepage cards style

--- index.php
+++ index.php
@@ -21,6 +21,20 @@
         <div class="header_nav_bar">Administrateur</div>
     </header>
 
+    <main>
+        <section class="first_section">
+            <div class="first_section_title">Devenez bénévole !</div>
+            <div class="first_section_images">
+                <img class="images_accueil_1"  src="./images/asset_images_page_accueil_2.jpg">
+                <img class="images_accueil_2"  src="./images/asset_images_page_accueil_3.jpg">
+                <img class="images_accueil_3"  src="./images/asset_images_page_accueil_4.jpg">
+                
+            </div>
+        </section>
+
+        <section class="card_section">
+    </main>
+
     <div class="card_container">
         <div class="card">
             <img src="./images/card_1.png" alt="homepage mission card">
@@ -47,19 +61,6 @@
             </div>
         </div>
     </div>
-    <main>
-        <section class="first_section">
-            <div class="first_section_title">Devenez bénévole !</div>
-            <div class="first_section_images">
-                <img class="images_accueil_1"  src="./images/asset_images_page_accueil_2.jpg">
-                <img class="images_accueil_2"  src="./images/asset_images_page_accueil_3.jpg">
-                <img class="images_accueil_3"  src="./images/asset_images_page_accueil_4.jpg">
-                
-            </div>
-        </section>
-
-        <section class="card_section">
-    </main>
 
 </body>
 
