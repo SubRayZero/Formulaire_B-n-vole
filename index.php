c35632b73bf479412e1f6f24fe468a337d20ede2
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-03-05 15:44:41 +0100

    Add - homepage cards

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Formulaire Bénévole</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./css/index.css">
     <script src="annonces.js" type="module"></script>
 </head>
 
@@ -14,7 +14,32 @@
     <header>
     </header>
 
-    <a href=""
+    <div class="card_container">
+        <div class="card">
+            <img src="./images/card_1.png" alt="homepage mission card">
+            <span>Actualité</span>
+            <div class="body_card">
+            <h2>Je participe au tri de la collecte nationale des restos du coeur</h2>
+            <p>Texte descriptif sur le bénévolat bla bla bla bla bla bla bla bla bla bla bla etc.................</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./images/card_2.png" alt="homepage mission card">
+            <span>Actualité</span>
+            <div class="body_card">
+            <h2>Je collecte des denrées pour les Restos du Coeur</h2>
+            <p>Texte descriptif sur le bénévolat bla bla bla bla bla bla bla bla bla bla bla etc.................</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./images/card_1.png" alt="homepage mission card">
+            <span>Actualité</span>
+            <div class="body_card">
+            <h2>Titre titre titre titre</h2>
+            <p>Texte descriptif sur le bénévolat bla bla bla bla bla bla bla bla bla bla bla etc.................</p>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
