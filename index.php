428b337f2b735799fc88e2951fe202a0c020e0a7
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-03-05 14:57:39 +0100

    css header+first section fini

--- index.php
+++ index.php
@@ -7,18 +7,29 @@
     <title>Formulaire Bénévole</title>
     <link rel="stylesheet" href="css/index.css">
     <script src="annonces.js" type="module"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
     <header class="header">
-        <div class="header_nav_bar">header</div>
+        <div class="header_nav_bar">Inscription</div>
+        <div class="header_nav_bar">Mon compte</div>
+        <div class="header_nav_bar">Administrateur</div>
     </header>
 
     <main>
         <section class="first_section">
             <div class="first_section_title">Devenez bénévole !</div>
-            <div class="first_section_images"></div>
+            <div class="first_section_images">
+                <img class="images_accueil_1"  src="./images/asset_images_page_accueil_2.jpg">
+                <img class="images_accueil_2"  src="./images/asset_images_page_accueil_3.jpg">
+                <img class="images_accueil_3"  src="./images/asset_images_page_accueil_4.jpg">
+                
+            </div>
         </section>
 
         <section class="card_section">
